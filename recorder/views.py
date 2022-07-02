bc26a18a41c594dddea60bb0ed28a20f7202a7a0
Tymophiyy <tima.matviiv@gmail.com>
2022-07-02 15:22:20 +0300

    now its working

--- recorder/views.py
+++ recorder/views.py
@@ -8,9 +8,21 @@ import os
 def index(request):
 	users = Child.objects.all()
 	context = {
-		"result": "error",
 		"users": users
 	}
+	
+	return render(request, "recorder/index.html", context=context)
+
+
+def add_child(request):
+	first_name = ""
+	last_name = ""
+	village = ""
+	ended_class = ""
+	parent_name = ""
+	parent_phone_number = ""
+	health_problems = ""
+
 	if request.method == "POST":
 		first_name = request.POST["first_name"]
 		last_name = request.POST["last_name"]
@@ -20,17 +32,59 @@ def index(request):
 		parent_phone_number = request.POST["parent_phone_number"]
 		health_problems = request.POST["health_problems"]
 
-		child = Child(first_name=first_name,
-			last_name=last_name,
-			village=village,
-			ended_class=ended_class,
-			parent_name=parent_name,
+		message_text = ""
+		if not first_name:
+			message_text = "Введіть будь ласка ім'я!"
+		elif not last_name:
+			message_text = "Введіть будь ласка прізвище"
+		elif not village:
+			message_text = "Введіть будь ласка місце проживання!"
+		elif not ended_class:
+			message_text = "Введіть будь ласка який клас закінчила дитина!"
+		elif not parent_name:
+			message_text = "Введіть будь ласка ім'я батьків!"
+		elif not parent_phone_number:
+			message_text = "Введіть будь ласка номер телефону батьків!"
+
+		if message_text:
+			context = {
+				"message": True,
+				"class": "danger",
+				"message_text": message_text,
+				"first_name": first_name,
+				"last_name": last_name,
+				"village": village,
+				"ended_class": ended_class,
+				"parent_name": parent_name,
+				"parent_phone_number": parent_phone_number,
+				"health_problems": health_problems
+			}
+
+			return render(request, "recorder/add.html", context=context)
+
+		child = Child(
+			first_name = first_name,
+			last_name = last_name,
+			village = village,
+			ended_class = ended_class,
+			parent_name = parent_name,
 			parent_phone_number = parent_phone_number,
 			health_problems = health_problems
 		)
 		child.save()
 
-		users = Child.objects.all()
-		return render(request, "recorder/index.html", context=context)
-
-	return render(request, "recorder/index.html", context=context)
+		first_name = ""
+		last_name = ""
+		village = ""
+		ended_class = ""
+		parent_name = ""
+		parent_phone_number = ""
+		health_problems = ""
+		
+		context = {
+			"message": True,
+			"class": "success",
+			"message_text": "Успішно додано!"
+		}
+		return render(request, "recorder/add.html", context=context)
+	return render(request, "recorder/add.html", context={"message": False})
