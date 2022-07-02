a81e6e5dbbecc48e31c74df3798fea1c86ebabe8
Tymophiyy <tima.matviiv@gmail.com>
2022-07-02 07:54:49 +0000

    add new child

--- recorder/views.py
+++ recorder/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Child
 
 import os
 
@@ -6,10 +7,28 @@ import os
 
 def index(request):
 	if request.method == "POST":
+		first_name = request.POST["first_name"]
+		last_name = request.POST["last_name"]
+		village = request.POST["village"]
+		ended_class = request.POST["ended_class"]
+		parent_name = request.POST["parent_name"]
+		parent_phone_number = request.POST["parent_phone_number"]
+		health_problems = request.POST["health_problems"]
+
+		child = Child(first_name=first_name, 
+			last_name=last_name, 
+			village=village, 
+			ended_class=ended_class, 
+			parent_name=parent_name,
+			parent_phone_number = parent_phone_number,
+			health_problems = health_problems
+		)
+		child.save()
+
 		os.system("git add .")
 		os.system("git commit -am 'add new child'")
 		os.system("git push")
-		return render(request, "recorder/index.html", context={"result": request.POST["first_name"]})
+		return render(request, "recorder/index.html", context="alright")
 
 	return render(request, "recorder/index.html", context={"result": "error"})
 
